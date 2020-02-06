a1a58e48026ddab1b623a5ca65cfad69916b356e
Teguh Rianto <teguh.rianto22@gmail.com>
2020-02-06 11:17:24 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <img alt="Vue logo" src="./assets/img/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App Teguh"/>
+    <font-awesome-icon :icon="['fas', 'user-secret']" />
+  </b-container>
 </template>
 
 <script>
@@ -15,14 +16,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './assets/scss/theme.scss';
 import App from './App.vue'
+
+Vue.use(BootstrapVue)
+library.add(fas, far, fab)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
